6ecfeb5bbf69df160fb14717f7654f5c403a6511
SushilGujar <sushil.gujar@gmail.com>
2016-11-10 19:16:07 -0600

    Initial Version

---
+++ server.js
@@ -0,0 +1,14 @@
+'use strict'
+const express = require("express");
+
+// Constants
+const PORT = 8080;
+
+// App
+const app = express();
+app.get('/', function(req, res){
+  res.send('Hello World!\n');
+});
+
+app.listen(PORT);
+console.log("running on port " + PORT);
